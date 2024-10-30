182eccf71dc7d0d82a0e4828b95375b19a284acd
Jorge0405 <23030515@itesa.edu.mx>
2024-10-30 07:25:42 -0600

    Version_02: Metodo Agregar ToString

--- ListaLigada.UI.Consola/Program.cs
+++ ListaLigada.UI.Consola/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using ListaLigada.Logica;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Lista ligada simple \n");
+
+        var singleList = new SingleList<string>();
+
+        singleList.Add("Juan");
+        singleList.Add("Jose");
+        singleList.Add("Jan");
+
+        Console.WriteLine(singleList);
     }
 }
